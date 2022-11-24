bf1361b2ee3adf9cfc8c33c8e20fdcee5bb97636
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-11-23 20:36:05 -0600

    fix: navigation loop caused by absolute path

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ function App() {
               )}
               <main className='page-container'>
                 <Route
-                  exact
                   path='/'
                   component={() =>
                     currentUser.isLoggedIn ? <Inventory /> : <Landing />
