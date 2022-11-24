05771aefd2ebd8df97d2e2a301e4f97012821367
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-11-23 21:04:52 -0600

    fix: incorrect use of history, changed to link

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Jumbotron, Button } from "react-bootstrap";
 
 export default function Landing() {
-  const history = useHistory();
   return (
     <Jumbotron style={{ margin: "10vh auto" }}>
       <h1 style={{ textAlign: "center" }}>Welcome to Tool Tracker</h1>
@@ -12,11 +11,11 @@ export default function Landing() {
         and report system.
       </p>
       <p style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Button
-          variant='outline-primary'
-          onClick={() => history.push("/login")}>
-          Sign in
-        </Button>
+        <Link to={"/login"}>
+          <Button variant='outline-primary'>
+            Sign in
+          </Button>
+        </Link>
       </p>
     </Jumbotron>
   );
